370ea2a137a6579d6749f15fd48a949db16ca001
Hui Ben <ben@find.xyz>
2023-07-02 19:38:52 -0400

    addition with input

--- sum/SumAB.java
+++ sum/SumAB.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class SumAB {
 	public static void main(String[] args) {
-		int a = 65;
-		int b = 100;
+		System.out.println("Please input 2 numbers for addition : ");
+		Scanner readInput = new Scanner(System.in);
+		int a = readInput.nextInt();
+		int b = readInput.nextInt();
+		readInput.close();
 
-		System.out.println(a + b);
+		System.out.println("The sum of the 2 numbers is : " + Integer.sum(a, b));
 	}
 }

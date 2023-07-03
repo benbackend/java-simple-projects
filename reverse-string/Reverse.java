3d16d7fe00af6bdfee33776927648d2499de61bf
Hui Ben <ben@find.xyz>
2023-07-02 20:04:18 -0400

    reverse using class function and recursion

--- reverse-string/Reverse.java
+++ reverse-string/Reverse.java
@@ -2,24 +2,18 @@
 
 public class Reverse{
 
-	public static void main(String[] args) {
-		String input = "123456";
-		int length = input.length();
-		char[] res = input.toCharArray();
-		for (int i = 0; i <= length / 2; i++) {
-			if (i == length / 2 ) {
-				if (length % 2 == 1) {
-					res[i] = input.charAt(i);
-				}
-				System.out.println(res);
-				return;
-			}
-			char head = input.charAt(i);
-			char tail = input.charAt(length - i - 1);
-
-			res[length - i - 1] = head;
-			res[i] = tail;
+	String reverse(String str) {
+		int length = str.length();
+		if (length == 0) {
+			return "";
 		}
+		return str.charAt(length - 1) + reverse(str.substring(0, length - 1));
+	}
+
+	public static void main(String[] args) {
+		String input = "qwerty";
+		Reverse rev = new Reverse();
+		System.out.println(rev.reverse(input));
 	}
 
 }
